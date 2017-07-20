38f16f084b97510f2790017a229b86b1e6e5fdc4
Ilya Seliverstov <ilya@amudo.com>
2017-07-20 10:58:44 +0200

    remove pods files update gitignore

--- Example/Pods/Target Support Files/FBAnnotationClusteringSwift/FBAnnotationClusteringSwift-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double FBAnnotationClusteringSwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char FBAnnotationClusteringSwiftVersionString[];
-
--- Example/Pods/Target Support Files/Pods-FBAnnotationClusteringSwift_Example/Pods-FBAnnotationClusteringSwift_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_FBAnnotationClusteringSwift_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_FBAnnotationClusteringSwift_ExampleVersionString[];
-
